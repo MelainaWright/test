298796d20d4cca97f3ffc1fda136a4a7d00ea457
Melaina Wright <mwright@bren.ucsb.edu>
2017-04-24 18:48:01 -0700

    changed things

--- sun.simpleexample.R
+++ sun.simpleexample.R
@@ -1,11 +1,5 @@
 sunlight = read.table("sun.txt", header=T)
 par(mar=c(5,6,3,2))
 boxplot(sunlight$Kdown_direct~sunlight$month,
-<<<<<<< HEAD
         ylab="Downwelling Solar\n kj/m2/day",
         xlab="month", col="red")
-=======
-  ylab="Downwelling Solar\n kj/m2/day",
-  xlab="month", col="red")
->>>>>>> 3841c6502ac8de1c69d7c1a95ba19a0fe894913c
-
