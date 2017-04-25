42ac2877fe8cf3dc0688c8d9bb5c940489917993
Melaina Wright <mwright@bren.ucsb.edu>
2017-04-24 18:44:01 -0700

    pretty red graph

--- sun.simpleexample.R
+++ sun.simpleexample.R
@@ -1,11 +1,6 @@
-
 sunlight = read.table("sun.txt", header=T)
 par(mar=c(5,6,3,2))
 boxplot(sunlight$Kdown_direct~sunlight$month,
-  ylab="Downwelling Solar\n kj/m2/day",
-<<<<<<< HEAD
-  xlab="month", col="red")
-=======
-  xlab="month", col="blue")
->>>>>>> parent of a6cb372... made graph green
+        ylab="Downwelling Solar\n kj/m2/day",
+        xlab="month", col="red")
 
