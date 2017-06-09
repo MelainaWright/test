f467ba1c0c11abaa84c9590c3b915d1f742f313b
Melaina Wright <mwright@bren.ucsb.edu>
2017-06-09 08:43:06 -0700

    spatial lab

---
+++ spatial.R
@@ -0,0 +1,12 @@
+#####################################################################################
+#ESM 262
+#6/9/2017
+#spatial data
+
+#######################################################################################
+#field data (continuous thing like temperature) is represented by a raster
+
+#feature data (a single thing) is represented by a vector, point: line: polygon
+
+library(raster)
+library(rgdal)
