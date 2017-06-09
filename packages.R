25f1690e14d937a3585bf646d3be688322dc0447
Melaina Wright <mwright@bren.ucsb.edu>
2017-06-09 08:39:52 -0700

    packages lab

---
+++ packages.R
@@ -0,0 +1,8 @@
+#################################################
+#ESM 262
+#6/2/2017
+#making packages that holds multiple functions and data
+
+######################################################
+#go to install and then install from to install packages you downloaded off the web
+
